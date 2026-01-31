1c66167124fc0feb0817d707029dacc5169fb553
Basareom <135543193+Basareom@users.noreply.github.com>
2026-01-31 17:36:20 +0530

    Update 10-Installation.sh

---
+++ 10-Installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-practice"
+LOGS_FILE="/var/log/shell-practice/$0.log"
+
+if [ $USERID -ne 0 ]; then
+   echo "Please run this script wit Root user access" | tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+       echo "$2...FAILURE" | tee -a $LOGS_FILE
+       exit 1
+    else
+       echo "$2...SUCCESS" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $? "Installing MySQL"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing NodeJS"
